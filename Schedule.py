1cbc40c82bbb5e45681bd9b9043b29742516095e
ittussaromYno3 <computer@Anfunfuns-MacBook-Air.local>
2020-07-01 15:03:41 -0400

    PLANNNNN

--- Schedule.py
+++ Schedule.py
@@ -10,11 +10,11 @@ then its a float
 THEN it will ask you what NAME is it
 and what id
 and will sort the floats (optional: when you type "sync")
-then will add the text ($TIME $NAME) to a  list
+then will add the text (<TIME> <name>) to a list
 `list` command shows time id and name
 `output` (or render?) iterates over the list and will display a tkinter Listbox that has that text
-`save $FILENAME` will save it as filename
-to process commands with arguments e.g. `save $FILENAME` it will run command.split("save ", "") and that would be the filename
+`save <FILENAME>` will save it as filename
+to process commands with arguments e.g. `save <FILENAME>` it will run command.split("save ", "") and that would be the filename
 AND THATS IT
 MAYBE IN FUTURE THIS COULD BE GUI, IT DOESNT SOUND TOO HARD???
 like the `list` command could show in a Toplevel and it will only render whenever you click `Render' or `output'
